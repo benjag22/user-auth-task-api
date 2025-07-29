84537d9fef0ced7952e483f1e755fbe27bd178cb
benjag22 <benjagonzalez2022@udec.cl>
2025-07-29 16:25:13 -0400

    first commit
    Upload Springboot project

---
+++ src/test/java/com/example/auth_task_api/AuthTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
