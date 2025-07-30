dc3a83a86780f1a9a157bfdd3cca67dce37677b3
benjag22 <benjagonzalez2022@udec.cl>
2025-07-29 20:24:00 -0400

    refactor: move ping controller to controllers folder

--- src/main/java/com/example/auth_task_api/controllers/Ping.java
+++ src/main/java/com/example/auth_task_api/controllers/Ping.java
@@ -1,6 +1,5 @@
-package com.example.auth_task_api;
+package com.example.auth_task_api.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
