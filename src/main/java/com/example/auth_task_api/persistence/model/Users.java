fd2f56f12f2e6d7852059f2ca9202aeee6a8787e
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:59:20 -0400

    feat: Users model implementation

--- src/main/java/com/example/auth_task_api/persistence/model/Users.java
+++ src/main/java/com/example/auth_task_api/persistence/model/Users.java
@@ -1,4 +1,41 @@
 package com.example.auth_task_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "first_name", length = 100, nullable = false)
+    private String firstName;
+
+    @Column(name = "middle_name", length = 100)
+    private String middleName;
+
+    @Column(name = "last_name", length = 100, nullable = false)
+    private String lastName;
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private UserLoginData userLoginData;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<UserHasRole> roles = new LinkedHashSet<>();
+
 }
