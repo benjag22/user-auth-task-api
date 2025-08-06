73cd5efdfe5cde29a95a382701ef6bd94dcf9449
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:47:46 -0400

    feat: Permission model implementation

--- src/main/java/com/example/auth_task_api/persistence/model/Permission.java
+++ src/main/java/com/example/auth_task_api/persistence/model/Permission.java
@@ -1,4 +1,33 @@
 package com.example.auth_task_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "grantedRoles")
+@ToString(exclude = "grantedRoles")
 public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 128)
+    private String description;
+
+    @OneToMany(mappedBy = "permission", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<RoleGrantedPermission> grantedRoles = new LinkedHashSet<>();
+
 }
