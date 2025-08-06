5bf143bbee2cf1c8dba51ff1a5dcc0628a1d0f74
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:53:30 -0400

    feat: composite primary key consisting of role and permission relationship

---
+++ src/main/java/com/example/auth_task_api/persistence/model/RoleGrantedPermissionPK.java
@@ -0,0 +1,8 @@
+package com.example.auth_task_api.persistence.model;
+import java.io.Serializable;
+
+public class RoleGrantedPermissionPK implements Serializable {
+    private Short role;
+    private Short permission;
+}
+
