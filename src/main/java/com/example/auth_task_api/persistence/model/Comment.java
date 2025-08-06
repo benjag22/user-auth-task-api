7900d7ecba400a413dda46dae20857f441441b9c
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:47:07 -0400

    feat: Comment model

--- src/main/java/com/example/auth_task_api/persistence/model/Comment.java
+++ src/main/java/com/example/auth_task_api/persistence/model/Comment.java
@@ -1,4 +1,22 @@
 package com.example.auth_task_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "description", length = 256)
+    private String description;
+
 }
