998fb05d41fad80c6a6ba45790f60a24ef6c6b95
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:57:27 -0400

    feat: Add model by role and permission relationship implementation

--- src/main/java/com/example/auth_task_api/persistence/model/RoleGrantedPermission.java
+++ src/main/java/com/example/auth_task_api/persistence/model/RoleGrantedPermission.java
@@ -1,4 +1,28 @@
 package com.example.auth_task_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "role_granted_permission")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"role", "permission"})
+@IdClass(RoleGrantedPermissionPK.class)
+
 public class RoleGrantedPermission {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "permission_id")
+    private Permission permission;
+
+
 }
