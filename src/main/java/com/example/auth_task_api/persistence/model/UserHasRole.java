a034cbae11842d5b63a7b1de8ef94a559f03e1f8
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:56:18 -0400

    feat: Add model by user and role relationship implementation

--- src/main/java/com/example/auth_task_api/persistence/model/UserHasRole.java
+++ src/main/java/com/example/auth_task_api/persistence/model/UserHasRole.java
@@ -1,4 +1,25 @@
 package com.example.auth_task_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_has_role")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@IdClass(UserHasRolePK.class)
 public class UserHasRole {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
