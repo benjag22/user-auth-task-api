e9ff1efe3ae0a013333280572d59d08d9002d3dc
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:47:59 -0400

    feat: Role model implementation

--- src/main/java/com/example/auth_task_api/persistence/model/Role.java
+++ src/main/java/com/example/auth_task_api/persistence/model/Role.java
@@ -1,4 +1,36 @@
 package com.example.auth_task_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "grantedPermissions")
+@ToString(exclude = "grantedPermissions")
+
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 128)
+    private String description;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private Set<RoleGrantedPermission> grantedPermissions  = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private Set<UserHasRole> users  = new LinkedHashSet<>();
 }
