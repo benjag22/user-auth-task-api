95dbe8db8b583189f536e0e821f6287b75c5211b
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 21:28:48 -0400

    !feat: Unnecessary injection

--- src/main/java/com/example/auth_task_api/service/UsersService.java
+++ src/main/java/com/example/auth_task_api/service/UsersService.java
@@ -2,12 +2,11 @@ package com.example.auth_task_api.service;
 
 import com.example.auth_task_api.persistence.model.Users;
 import com.example.auth_task_api.persistence.repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
-    @Autowired
+
     private UsersRepository usersRepository;
 
     public Users registerUser(Users user) {
