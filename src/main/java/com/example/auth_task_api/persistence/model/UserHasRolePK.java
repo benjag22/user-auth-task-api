ed2d914da74956b520a86b491dfaae6e317505e9
benjag22 <benjagonzalez2022@udec.cl>
2025-08-05 20:53:51 -0400

    feat: composite primary key consisting of role and user relationship

---
+++ src/main/java/com/example/auth_task_api/persistence/model/UserHasRolePK.java
@@ -0,0 +1,8 @@
+package com.example.auth_task_api.persistence.model;
+
+import java.io.Serializable;
+
+public class UserHasRolePK implements Serializable {
+    private short user;
+    private short role;
+}
