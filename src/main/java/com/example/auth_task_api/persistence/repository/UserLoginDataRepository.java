e3da415a5f75801833d3733dcb770ec4a5d14b04
benjag22 <benjagonzalez2022@udec.cl>
2025-08-08 00:47:19 -0400

    feat: create basic UserLoginData repository

---
+++ src/main/java/com/example/auth_task_api/persistence/repository/UserLoginDataRepository.java
@@ -0,0 +1,9 @@
+package com.example.auth_task_api.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserLoginDataRepository extends JpaRepository<UserLoginDataRepository, Long> {
+
+}
