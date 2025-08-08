6ab0723b7e566534db8f22d48670fbc09be1049a
benjag22 <benjagonzalez2022@udec.cl>
2025-08-08 00:46:58 -0400

    fix: solve Infinite redirect between User nad userLoginData models

--- src/main/java/com/example/auth_task_api/persistence/model/Users.java
+++ src/main/java/com/example/auth_task_api/persistence/model/Users.java
@@ -9,8 +9,6 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,18 +16,27 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long Id;
+    @Getter
+    private Long id;
 
     @Column(name = "creation_date")
+    @Getter
+    @Setter
     private LocalDateTime creationDate;
 
     @Column(name = "first_name", length = 100, nullable = false)
+    @Getter
+    @Setter
     private String firstName;
 
     @Column(name = "middle_name", length = 100)
+    @Getter
+    @Setter
     private String middleName;
 
     @Column(name = "last_name", length = 100, nullable = false)
+    @Getter
+    @Setter
     private String lastName;
 
     @OneToOne(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
