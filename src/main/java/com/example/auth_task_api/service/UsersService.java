3eb4b6d07ff7bdf49adec396604f7d358fb6ae22
benjag22 <benjagonzalez2022@udec.cl>
2025-08-08 00:48:04 -0400

    feat: Add method to register user with Dto

--- src/main/java/com/example/auth_task_api/service/UsersService.java
+++ src/main/java/com/example/auth_task_api/service/UsersService.java
@@ -1,15 +1,36 @@
 package com.example.auth_task_api.service;
 
+import com.example.auth_task_api.api.dto.Users.UsersCreateRequestDto;
 import com.example.auth_task_api.persistence.model.Users;
 import com.example.auth_task_api.persistence.repository.UsersRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
 
-    private UsersRepository usersRepository;
+    private final UsersRepository usersRepository;
 
-    public Users registerUser(Users user) {
-        return usersRepository.save(user);
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+    @Transactional
+    public void registerUser(UsersCreateRequestDto userDto) {
+        String firstName = userDto.getFirstName();
+        String lastName = userDto.getLastName();
+        String middleName = null;
+        if (userDto.getMiddleName() != null && !userDto.getMiddleName().trim().isEmpty()) {
+            middleName = userDto.getMiddleName().trim();
+        }
+
+        Users newUser = Users.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .middleName(middleName)
+                .build();
+
+        Users savedUser = usersRepository.save(newUser);
     }
 }
