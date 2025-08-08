7c0806b3523a89a882fc655bd050d238e6be90b9
benjag22 <benjagonzalez2022@udec.cl>
2025-08-08 00:09:34 -0400

    feat: Create user dto

---
+++ src/main/java/com/example/auth_task_api/api/dto/Users/UsersCreateRequestDto.java
@@ -0,0 +1,30 @@
+package com.example.auth_task_api.api.dto.Users;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsersCreateRequestDto {
+
+    @NotBlank(message = "The first name cannot be empty")
+    private String firstName;
+
+    private String middleName;
+
+    @NotBlank(message = "The last name cannot by empty")
+    private String lastName;
+
+    @Email(message = "Email should be valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    private String email_adress;
+
+    private String nickname;
+
+    @NotBlank(message = "cannot be empty")
+    @Size(min = 7, max = 15)
+    private String password;
+
+}
