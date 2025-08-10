bb56e059892d6207a181f110507aa379c0d85b52
benjag22 <benjagonzalez2022@udec.cl>
2025-08-10 00:13:14 -0400

    feat: create WebSecurityConfig

---
+++ src/main/java/com/example/auth_task_api/Security/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.auth_task_api.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/users/create").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
