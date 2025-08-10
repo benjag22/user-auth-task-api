f1b119705fd2c7f6b94b61d3db5eb0f71fea4895
benjag22 <benjagonzalez2022@udec.cl>
2025-08-09 21:59:42 -0400

    refactor: Move static resources and configuration to .yml

--- src/main/java/com/example/auth_task_api/Security/SecurityConstrants.java
+++
@@ -1,8 +0,0 @@
-package com.example.auth_task_api.Security;
-
-public class SecurityConstrants {
-    public static final long EXPIRATION_TIME = 432_000_000; // 5 days
-    public static final String SECRET = "oursecretkey";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-}
