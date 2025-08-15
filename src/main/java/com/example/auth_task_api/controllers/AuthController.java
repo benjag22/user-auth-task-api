68bf54d1cf1389ebc317cc72d88ae6ef28cb2d27
benjag22 <benjagonzalez2022@udec.cl>
2025-08-14 22:35:05 -0400

    feat: add login endpoint

--- src/main/java/com/example/auth_task_api/controllers/AuthController.java
+++ src/main/java/com/example/auth_task_api/controllers/AuthController.java
@@ -1,11 +1,24 @@
 package com.example.auth_task_api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.auth_task_api.api.dto.Auth.TokenResponse;
+import com.example.auth_task_api.api.dto.Users.UserLoginRequestDto;
+import com.example.auth_task_api.service.UserLoginDataService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    UserLoginDataService userLoginDataService;
 
+    AuthController(UserLoginDataService userLoginDataService) {
+        this.userLoginDataService = userLoginDataService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+        final TokenResponse token = userLoginDataService.attempLogin(userLoginRequestDto);
+        return ResponseEntity.ok(token);
+    }
 
 }
